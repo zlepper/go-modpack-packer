2d04eb01de50e935a5eae4b100379c6f928389d1
zlepper <hansen13579@gmail.com>
2016-09-30 17:58:55 +0200

    Unbreak solder tests. Also know as: Hardcoding another value. Or: Your ids change when you delete your database

--- source/backend/solder/solderClient_test.go
+++ source/backend/solder/solderClient_test.go
@@ -1,8 +1,8 @@
 package solder
 
 import (
-	"testing"
 	"github.com/zlepper/go-modpack-packer/source/backend/types"
+	"testing"
 )
 
 func getClient() *SolderClient {
@@ -15,9 +15,9 @@ func getClient() *SolderClient {
 func TestSolderClient_GetModId(t *testing.T) {
 	client := getClient()
 
-	id := client.GetModId("agricraft")
-	if id != "508" {
-		t.Errorf("Expected mod id 508, got %s", id)
+	id := client.GetModId("ae2stuff")
+	if id != "771" {
+		t.Errorf("Expected mod id 771, got %s", id)
 	}
 
 	id = client.GetModId("teknflganfgcraft")
@@ -30,19 +30,19 @@ func TestSolderClient_GetModVersionId(t *testing.T) {
 	client := getClient()
 
 	modVersionId := client.GetModVersionId(&types.OutputInfo{
-		Id:"agricraft",
-		MinecraftVersion:"1.7.10",
-		Version:"1.5.0",
+		Id:               "ae2stuff",
+		MinecraftVersion: "1.7.10",
+		Version:          "0.5.1.9",
 	})
 
-	if modVersionId != "1503" {
-		t.Errorf("Expected mod version id 1503m got %s", modVersionId)
+	if modVersionId != "1912" {
+		t.Errorf("Expected mod version id 1912 got %s", modVersionId)
 	}
 
 	modVersionId = client.GetModVersionId(&types.OutputInfo{
-		Id:"acricraft",
-		MinecraftVersion:"1.7.10",
-		Version:"1.4.0",
+		Id:               "ae2stuff",
+		MinecraftVersion: "1.7.10",
+		Version:          "1.4.0",
 	})
 
 	if modVersionId != "" {
@@ -54,9 +54,9 @@ func TestSolderClient_IsModversionOnline(t *testing.T) {
 	client := getClient()
 
 	exists := client.IsModversionOnline(&types.OutputInfo{
-		Id:"agricraft",
-		MinecraftVersion:"1.7.10",
-		Version:"1.5.0",
+		Id:               "agricraft",
+		MinecraftVersion: "1.7.10",
+		Version:          "1.5.0",
 	})
 
 	if !exists {
@@ -64,9 +64,9 @@ func TestSolderClient_IsModversionOnline(t *testing.T) {
 	}
 
 	exists = client.IsModversionOnline(&types.OutputInfo{
-		Id:"acricraft",
-		MinecraftVersion:"1.7.10",
-		Version:"1.4.0",
+		Id:               "acricraft",
+		MinecraftVersion: "1.7.10",
+		Version:          "1.4.0",
 	})
 
 	if exists {
