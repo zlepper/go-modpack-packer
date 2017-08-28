5cefe2c15f486786aa4b058cd25f5f3a9b7dc505
Zlepper <hansen13579@gmail.com>
2017-08-28 19:57:04 +0200

    Prevent accidental closing of info ui
    And save modpack before starting packing

--- source/frontend/src/app/technic/technic-settings/technic-settings.component.ts
+++ source/frontend/src/app/technic/technic-settings/technic-settings.component.ts
@@ -1,6 +1,7 @@
 import {Component, Input, OnInit} from "@angular/core";
 import {MdDialog} from "@angular/material";
 import {Modpack} from "app/models/modpack";
+import {ModpackService} from "app/services/modpack.service";
 import {TechnicBuildingComponent} from "app/technic/technic-building/technic-building.component";
 
 @Component({
@@ -13,14 +14,19 @@ export class TechnicSettingsComponent implements OnInit {
   @Input()
   public modpack: Modpack;
 
-  constructor(protected dialog: MdDialog) {
+  constructor(protected dialog: MdDialog, private modpackService: ModpackService) {
   }
 
   ngOnInit() {
   }
 
   public startBuild() {
-    const dialog = this.dialog.open(TechnicBuildingComponent);
+    // Save the modpack, so it's easy to restore the next time
+    this.modpackService.saveModpacks();
+
+    const dialog = this.dialog.open(TechnicBuildingComponent, {
+      disableClose: true
+    });
     dialog.afterClosed().subscribe(() => {
       console.log('closed');
     })
